edca98dca404001ed8134c565de59b9a24426a01
Merichu Duncan <chue_3@yahoo.com>
2017-02-12 14:19:03 -0500

    Added changes to Index/Layouts styles.

--- MVCReviewSiteProj/Models/Category.cs
+++ MVCReviewSiteProj/Models/Category.cs
@@ -7,5 +7,11 @@ namespace MVCReviewSiteProj.Models
 {
     public class Category
     {
+        public int CategoryID { get; set; }
+        public string Type { get; set; }
+
+        //Added cause this list would have many tasks
+        public virtual ICollection<Review> Reviews { get; set; }
+
     }
 }
--- MVCReviewSiteProj/Models/Review.cs
+++ MVCReviewSiteProj/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,36 @@ namespace MVCReviewSiteProj.Models
 {
     public class Review
     {
+        [Key]
+        //fields and properties
+        public int ID { get; set; }
+
+        //Product hierarchy
+        [Display(Name ="Brand")]
+        public string BrandName { get; set; }
+        [Display(Name ="Model")]
+        public string ModelName { get; set; }
+        [Display(Name ="Publish Date")]
+        public DateTime PublishDate { get; set; }
+
+        [Display(Name ="Name")]
+        public string UserName { get; set; }
+        [Display(Name ="Email Address")]
+        public string EmailAdd { get; set; }
+        [Display(Name ="Write your Review")]
+        public string Content { get; set; }
+        [Display(Name ="Over All Rating")]
+        public int overAllRate { get; set; }
+        [Display(Name ="Is this HelpFul?")]
+        public bool Helpful { get; set; }
+
+
+        //Asigning foreign key indorder to link classes in Models
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
     }
 }
